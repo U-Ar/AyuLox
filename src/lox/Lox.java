5bd71ebaf54cec28d3b9d7b53f4548bdb85472c8
yuarakaw <yuarakaw@lycorp.co.jp>
2025-07-07 15:20:52 +0900

    first commit

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+    private static final Interpreter interpreter = new Interpreter();
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,6 +29,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -51,6 +54,7 @@ public class Lox {
 
         if (hadError) return;
 
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -68,5 +72,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
